14cc99c4247adc0998aba04b48c14329eb0791bb
Yusuf2668 <yusufbilal159@gmail.com>
2022-02-14 20:56:40 +0300

    BandFrontPanelDone

--- Assets/Scripts/Yusuf/BantController.cs
+++ Assets/Scripts/Yusuf/BantController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,36 +7,67 @@ public class BantController : MonoBehaviour
 {
     public TakeOffAreaController takeOffAreaController;
     public List<GameObject> boxesList;
+    public float bantPeriodTime;
 
+    public float timer;
+    public Transform startPoint;
+
+    private void Start()
+    {
+        timer = 0;
+    }
     public bool canWork
     {
         get
         {
-            return takeOffAreaController.takeOffAreaBoxesLine >= 0;
+            return takeOffAreaController.takeOffAreaBoxesLine >= 0 && timer <= 0;
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (timer >= 0)
         {
-            Debug.Log(takeOffAreaController.takeOffAreaBoxesLine);
-            SetDeActiveTakeOffAreaBoxes();
+            timer -= Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+
+        if (canWork)
         {
-            SetActiveBoxes();
+            timer = bantPeriodTime;
+            MoveToStartPoint();
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        { 
+            other.gameObject.SetActive(false);
+        }
+    }
+    public void MoveToStartPoint()
+    {
+        takeOffAreaController.takeOffAreaBoxesList[takeOffAreaController.takeOffAreaBoxesLine].transform.DOMove(startPoint.position, 1f).OnComplete(()=>SetActiveBoxes());
     }
     public void SetActiveBoxes()
     {
+        boxesList[takeOffAreaController.takeOffAreaBoxesLine].gameObject.SetActive(true);
+        takeOffAreaController.takeOffAreaBoxesLine--;
+    }
+    public void SetDeActiveTakeOffAreaBoxes()
+    {
+        takeOffAreaController.takeOffAreaBoxesList[takeOffAreaController.takeOffAreaBoxesLine].SetActive(false);
+    }
+    public void SetActiveBoxesForRobots()
+    {
+        //içinde tekrardan kontrol etmem robot kollarý için
         if (canWork)
         {
             boxesList[takeOffAreaController.takeOffAreaBoxesLine].gameObject.SetActive(true);
-            
+
         }
     }
-    public void SetDeActiveTakeOffAreaBoxes()
+    public void SetDeActiveTakeOffAreaBoxesForRobot()
     {
         if (canWork)
         {
