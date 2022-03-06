16b1b80dc73e7b10e62144f8d7b8d0f5af41a525
HMertYAVA <hmertyavas@hotmail.com>
2022-03-06 17:45:20 +0300

    Money

--- Assets/Scripts/Mert/UpgradeObjects.cs
+++ Assets/Scripts/Mert/UpgradeObjects.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeObjects : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class UpgradeObjects : MonoBehaviour
     public GameObject upgradedObj;
     public ParticleSystem upgradeEffect;
 
+    public Text buyObjectText;
+    bool buyItemBool = true;
 
-
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (other.CompareTag("Player"))
+        buyObjectText.text = value.ToString();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (buyItemBool && other.CompareTag("Player"))
         {
             UpgradeControl();
         }
@@ -24,11 +31,26 @@ public class UpgradeObjects : MonoBehaviour
     {
         if (value <= MoneyController.instance.money)
         {
-            MoneyController.instance.BuyItem(value);
+            StartCoroutine(UIChanged());
+
+            if (value == 1)
+            {
             upgradeObj.SetActive(false);
             upgradedObj.SetActive(true);
+            gameObject.GetComponent<Collider>().enabled = false;
             upgradeEffect.Play();
+            }
+            buyItemBool = false;
         }
+    }
+
+        IEnumerator UIChanged()
+    {
+            MoneyController.instance.BuyItem();
+            yield return new WaitForSeconds(0.01f);
+            value--;
+            buyObjectText.text = value.ToString();
+            buyItemBool=true;
     }
 
 }
